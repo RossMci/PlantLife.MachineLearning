940500e93a2f049e9b12a7036233d293265acf97
Ross Mcinerney <62793400+RossMci@users.noreply.github.com>
2022-03-13 21:42:29 +0000

    Update runModel.py

--- NoteBooks/runModel.py
+++ NoteBooks/runModel.py
@@ -7,16 +7,15 @@ from tensorflow import keras
 import numpy as np
 import tensorflow as tf
 
-class_names=['daisy', 'dandelion', 'roses', 'sunflowers', 'tulips']
-READ_BINARY = "rb"
 
+ # loading saved model
 model_filepath="flower-ann-model.h5"
 model = keras.models.load_model(model_filepath)
 
+sunflower_path = sys.argv[1] # adjustment 1
+
 img_height = 180
 img_width = 180
-
-sunflower_path = sys.argv[1]
 
 img = tf.keras.utils.load_img(
     sunflower_path, target_size=(img_height, img_width)
@@ -28,8 +27,9 @@ predictions = model.predict(img_array)
 
 score = tf.nn.softmax(predictions[0])
 
+# adjustment 2
+class_names=['daisy', 'dandelion', 'roses', 'sunflowers', 'tulips']
+
 json_result_dictonary = {'class': class_names[np.argmax(score)], 'score': 100 * np.max(score)}
-
-#print(    "This image most likely belongs to {} with a {:.2f} percent confidence."     .format(class_names[np.argmax(score)], 100 * np.max(score)))
-
 print(json_result_dictonary)
+#print(    "This image most likely belongs to {} with a {:.2f} percent confidence."     .format(class_names[np.argmax(score)], 100 * np.max(score)))
