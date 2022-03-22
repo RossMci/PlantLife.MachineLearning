6c2582a52c89e53b9be49045775ed5a3ec92d9af
Ross Mcinerney <62793400+RossMci@users.noreply.github.com>
2022-03-22 20:45:30 +0000

    removed unnecessary files

--- plantLifeApp/app/src/main/java/com/example/android/plantlifeapp/Adapters/mySnapAdapter.java
+++
@@ -1,4 +0,0 @@
-package com.example.android.plantlifeapp.Adapters;
-
-public class mySnapAdapter {
-}
--- plantLifeApp/app/src/main/java/com/example/android/plantlifeapp/List.java
+++
@@ -1,7 +0,0 @@
-package com.example.android.plantlifeapp;
-
-import java.util.ArrayList;
-
-public interface List {
-    void push(ArrayList<Bakery> listGroup);
-}
